997e60f71c990fe8967c2cb2d6a77a854c9ffdb2
caozongli <932478335@qq.com>
2021-07-14 21:48:23 +0800

    2021.07.14

--- Iterator/IteratorGP.cpp
+++ Iterator/IteratorGP.cpp
@@ -1,38 +1,202 @@
-template<typename T>
-class MyCollection
+#include <iostream>
+#include <vector>
+#include <list>
+#include <time.h>
+#include <typeinfo>
+using namespace std;
+
+
+typedef struct iterator_tag {} iterator_tag;
+typedef struct vector_iterator_tag : public iterator_tag {} vector_iterator_tag;
+typedef struct list_iterator_tag : public iterator_tag {} list_iterator_tag;
+
+
+template <typename T>
+class iterator_traitsmy
 {
-	typename ... const_iterator;
+public:
+	typedef typename T::iterator_category iterator_category;
+};
 
-	const_iterator first() const{}
 
-	const_iterator next() const{}
 
-	const_iterator hasNext() const{}
-
-	const_iterator& currentItem() const{}
-
-}
-
-template<typename T>
-class Mycollection2
+template<typename T, typename category>
+class Iterator
 {
-	typename ... const_iterator;
+public:
+	typedef category iterator_category;
+	virtual T begin() = 0;
+	virtual T next() = 0;
+	virtual bool isEnd() const = 0;
+	virtual T currentItem() const = 0;
+};
 
-	const_iterator first() const{}
 
-	const_iterator next() const{}
-
-	const_iterator hasNext() const{}
-
-	const_iterator currentItem() const{}
-}
-
-template<typename T>
-void MyAlgorithm(T const & coll)
+template<typename T, typename category>
+class VectorCollectionIterator:public Iterator<T, category>
 {
-	T::const_iterator iter;
-	for(iter=coll.first(); iter!=coll.hasNext; iter=coll.hasNext())
-	{
-		
+private:
+	vector<T> mc;
+	int posi;
+public:
+	VectorCollectionIterator(vector<T> c):mc(c), posi(-1){}
+
+	T begin(){
+		if(mc.empty()) return {};
+		posi = 0;
+		return mc[posi];
 	}
+
+	T next(){
+		if(isEnd()) return {};
+		return mc[++posi];
+	}
+
+	bool isEnd() const {
+		if(posi == mc.size()-1) return true;
+		return false;
+	}
+
+	T currentItem() const {
+		if(-1 == posi) return {};
+		return mc[posi];
+	}
+
+};
+
+
+template<typename T, typename category>
+class VectorCollection
+{
+private:
+	vector<T> mc;
+public:
+
+	VectorCollection(vector<T> c): mc(c){}
+
+	VectorCollectionIterator<T, category>* GetIterator()
+	{
+		return new VectorCollectionIterator<T, category>(mc);
+	}
+};
+
+
+template<typename T, typename category>
+class ListCollectionIterator:public Iterator<T, category>
+{
+private:
+	list<T> mc;
+	typename list<T>::iterator posi;
+public:
+	ListCollectionIterator(list<T> &c):mc(c){ posi= mc.begin();}
+	T begin(){
+		if(mc.empty()) return {};
+		posi = mc.begin();
+		return *(mc.begin());
+	}
+
+	T next(){
+		if(isEnd()) return {};
+		return *(++posi);
+	}
+
+	bool isEnd() const {
+		if(posi == mc.end()) return true;
+		return false;
+	}
+
+	T currentItem() const {
+		return *posi;
+	}
+
+};
+
+
+
+template<typename T, typename category>
+class ListCollection
+{
+private:
+	list<T> mc;
+public:
+
+	ListCollection(list<T> mc): mc(mc){}
+
+	ListCollectionIterator<T, category>* GetIterator()
+	{
+		return new ListCollectionIterator<T, category>(mc);
+	}
+};
+
+
+template<class Iterator>
+void __scan(Iterator* iter, iterator_tag p)
+{
+	for(iter->begin(); !iter->isEnd(); iter->next())
+	{
+		auto a = iter->currentItem();
+	}
+}
+
+
+template<class VectorCollectionIterator>
+void __scan(VectorCollectionIterator* iter, vector_iterator_tag p)
+{
+	for(iter->begin(); !iter->isEnd(); iter->next())
+	{
+		auto a = iter->currentItem();
+	}
+}
+
+
+template<class ListCollectionIterator>
+void __scan(ListCollectionIterator* iter, list_iterator_tag p)
+{
+	for(iter->begin(); !iter->isEnd(); iter->next())
+	{
+		auto a = iter->currentItem();
+	}
+}
+
+
+template<class iterator>
+void scan(iterator* iter)
+{
+	typedef typename iterator_traitsmy<iterator>::iterator_category c;
+	__scan(iter, c());
+}
+
+
+void myAlgorithm()
+{
+	vector<int> vectorcollect;
+	list<int> listcollect;
+	for(int i=1; i<10000000; i++)
+	{
+		vectorcollect.push_back(i);
+		listcollect.push_back(i);
+	}
+
+
+	VectorCollection<int, vector_iterator_tag> mcV(vectorcollect);
+	VectorCollectionIterator<int, vector_iterator_tag> *iterV = mcV.GetIterator();
+
+	ListCollection<int, list_iterator_tag> mcL(listcollect);
+	ListCollectionIterator<int, list_iterator_tag> *iterL = mcL.GetIterator();
+
+	clock_t startTime, endTime;
+	startTime = clock(); //计时开始
+
+	scan(iterV);
+	scan(iterL);	
+
+	endTime = clock(); //计时结束
+	cout << "The run time is:" << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
+}
+
+int main()
+{
+	myAlgorithm();
+	
+	return 0;
 }
--- Iterator/iteratorOOP.cpp
+++ Iterator/iteratorOOP.cpp
@@ -1,57 +1,159 @@
 #include <iostream>
+#include <vector>
+#include <list>
+#include <time.h>
 using namespace std;
 
 template<typename T>
 class Iterator
 {
 public:
-	virtual void first() = 0;
-	virtual void next() = 0;
-	virtual bool hasNext() const = 0;
-	virtual T& currentItem() = 0;
+	virtual T begin() = 0;
+	virtual T next() = 0;
+	virtual bool isEnd() const = 0;
+	virtual T currentItem() const = 0;
 };
 
-template<typename T>
-class MyCollection
-{
-	//....
-	Iterator<T> GetIterator()
-	{
-
-	};
-};
 
 template<typename T>
-class CollectionIterator:public Iterator<T>
+class VectorCollectionIterator:public Iterator<T>
 {
-	MyCollection<T> mc;
+private:
+	vector<T> mc;
+	int posi;
 public:
-	CollectionIterator(const MyCollection<T>&c):mc(c){}
-	void first() override{
+	VectorCollectionIterator(vector<T> c):mc(c), posi(-1){}
 
-	}
-	void next() override{
-
-	}
-	bool hasNext() const override{
-
-	}
-	T& currentItem() override{
-
+	T begin(){
+		if(mc.empty()) return {};
+		posi = 0;
+		return mc[posi];
 	}
 
+	T next(){
+		if(isEnd()) return {};
+		return mc[++posi];
+	}
+
+	bool isEnd() const {
+		if(posi == mc.size()-1) return true;
+		return false;
+	}
+
+	T currentItem() const {
+		if(-1 == posi) return {};
+		return mc[posi];
+	}
+
+};
+
+
+template<typename T>
+class VectorCollection
+{
+private:
+	vector<T> mc;
+public:
+
+	VectorCollection(vector<T> c): mc(c){}
+
+	VectorCollectionIterator<T>* GetIterator()
+	{
+		return new VectorCollectionIterator<T>(mc);
+	}
+};
+
+
+template<typename T>
+class ListCollectionIterator:public Iterator<T>
+{
+private:
+	list<T> mc;
+	typename list<T>::iterator posi;
+public:
+	ListCollectionIterator(list<T> &c):mc(c){ posi= mc.begin();}
+
+	T begin(){
+		if(mc.empty()) return {};
+		posi = mc.begin();
+		return *(mc.begin());
+	}
+
+	T next(){
+		if(isEnd()) return {};
+		return *(++posi);
+	}
+
+	bool isEnd() const {
+		if(posi == mc.end()) return true;
+		return false;
+	}
+
+	T currentItem() const {
+		return *posi;
+	}
+
+};
+
+
+
+template<typename T>
+class ListCollection
+{
+private:
+	list<T> mc;
+public:
+
+	ListCollection(list<T> mc): mc(mc){}
+
+	ListCollectionIterator<T>* GetIterator()
+	{
+		return new ListCollectionIterator<T>(mc);
+	}
+};
+
+template <typename T>
+void scan(Iterator<T>* iter)
+{
+	for(iter->begin(); !iter->isEnd(); iter->next())
+	{
+		auto a = iter->currentItem();
+	}
 }
+
 
 void myAlgorithm()
 {
-	MyCollection<int> mc;
-	Iterator<int> iter = mc.GetIterator();
-
-	for(iter.first(); !iter.isDone(); iter.next())
+	vector<int> vectorcollect;
+	list<int> listcollect;
+	for(int i=1; i<10000000; i++)
 	{
-		cout << iter.current() << endl;
+		vectorcollect.push_back(i);
+		listcollect.push_back(i);
 	}
+
+
+	VectorCollection<int> mcV(vectorcollect);
+	VectorCollectionIterator<int> *iterV = mcV.GetIterator();
+
+	ListCollection<int> mcL(listcollect);
+	ListCollectionIterator<int> *iterL = mcL.GetIterator();
+
+
+	clock_t startTime, endTime;
+	startTime = clock(); //计时开始
+
+	scan(iterV);
+	scan(iterL);	
+
+	endTime = clock(); //计时结束
+	cout << "The run time is:" << (double)(endTime - startTime) / CLOCKS_PER_SEC << "s" << endl;
 }
 
+int main()
+{
+	myAlgorithm();
+	return 0;
+}
 
 
